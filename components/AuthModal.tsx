65e5960990dd53d7dc09fb96f3993712dbb71cc1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-30 15:43:35 +0530

    added style to auth modal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,17 +1,52 @@
 "use client";
 
 import React from "react";
+import {
+  useSupabaseClient,
+  useSessionContext
+} from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+
 import Modal from "./Modal";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+import useAuthModal from "@/hooks/useAuthModal";
 
 const AuthModal = () => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) onClose();
+  };
+
   return (
     <Modal
       title="Welcome Back"
       description="Log In to Your Account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
-      AuthModalChildren
+      <Auth
+        theme="dark"
+        providers={["github"]}
+        magicLink
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e"
+              }
+            }
+          }
+        }}
+      />
     </Modal>
   );
 };
