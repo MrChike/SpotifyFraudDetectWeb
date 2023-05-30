8aa52200f1a6c889940af5d1508f45149c98d61b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-30 20:09:49 +0530

    installed react-hook-form, uniqid

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,7 +1,29 @@
+"use client";
+
 import React from "react";
 
+import Modal from "./Modal";
+import useUploadModal from "@/hooks/useUploadModal";
+
 const UploadModal = () => {
-  return <div>UploadModal</div>;
+  const uploadModal = useUploadModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      uploadModal.onClose();
+    }
+  };
+
+  return (
+    <Modal
+      title="Add a Song"
+      description="Upload an MP3 File."
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      Form
+    </Modal>
+  );
 };
 
 export default UploadModal;
