e6c1f64dbc9c2ca71372483804b28cddbb4131df
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-31 22:22:03 +0530

    added new component: PlayerContent

---
+++ components/PlayerContent.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+
+import { Song } from "@/types";
+
+interface PlayerContentProps {
+  song: Song;
+  songUrl: string;
+}
+
+const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
+  return <div>PlayerContent</div>;
+};
+
+export default PlayerContent;
