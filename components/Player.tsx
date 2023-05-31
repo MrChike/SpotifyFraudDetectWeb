4b000a43cff7ff767c95a7f295b85e9d749d74d8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-31 21:54:43 +0530

    created new hook: useLoadSongUrl

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,9 +3,14 @@
 import React from "react";
 
 import usePlayer from "@/hooks/usePlayer";
+import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 
 const Player = () => {
   const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+
+  const songUrl = useLoadSongUrl(song!);
 
   return <div>Player</div>;
 };
