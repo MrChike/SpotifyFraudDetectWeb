a978a25196de83d3dbed50b79d4028f90cb3b9b9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-31 13:28:00 +0530

    created new component: SearchContent

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,7 @@
-import getSongsByTitle from "@/actions/getSongsByTitle";
-
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
+import getSongsByTitle from "@/actions/getSongsByTitle";
 
 interface SearchProps {
   searchParams: {
@@ -20,6 +20,7 @@ const Search = async ({ searchParams }: SearchProps) => {
           <SearchInput />
         </div>
       </Header>
+      <SearchContent songs={songs} />
     </div>
   );
 };
