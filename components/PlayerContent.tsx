c685c820e55770395fb8581a6b86ea7715760612
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-31 23:04:57 +0530

    added media & volume control icons

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import React from "react";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 
 import { Song } from "@/types";
+
+import MediaItem from "./MediaItem";
+import LikeButton from "./LikeButton";
 
 interface PlayerContentProps {
   song: Song;
@@ -10,7 +16,53 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
-  return <div>PlayerContent</div>;
+  const Icon = true ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 h-full">
+      <div className="flex w-full justify-start">
+        <div className="flex items-center gap-x-4">
+          <MediaItem data={song} />
+          <LikeButton songId={song.id} />
+        </div>
+      </div>
+
+      <div className="flex md:hidden col-auto w-full justify-end items-center">
+        <div
+          onClick={() => {}}
+          className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
+        >
+          <Icon size={30} className="text-black" />
+        </div>
+      </div>
+
+      <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
+        <AiFillStepBackward
+          size={30}
+          onClick={() => {}}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+        <div
+          onClick={() => {}}
+          className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
+        >
+          <Icon size={30} className="text-black" />
+        </div>
+        <AiFillStepForward
+          size={30}
+          onClick={() => {}}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon onClick={() => {}} className="cursor-pointer" size={34} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlayerContent;
