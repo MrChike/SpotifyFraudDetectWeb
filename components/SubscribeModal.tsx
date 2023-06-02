decf7a34a498f7f6ca9f336d3f0124265af1e982
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-02 20:40:55 +0530

    added subscribe button props

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
 
 import Modal from "./Modal";
 import Button from "./Button";
 
 import { Price, ProductWithPrice } from "@/types";
+import { useUser } from "@/hooks/useUser";
+import { postData } from "@/libs/helpers";
+import { getStripe } from "@/libs/stripeClient";
 
 interface SubscribeModalProps {
   products: ProductWithPrice[];
@@ -22,6 +26,38 @@ const formatPrice = (price: Price) => {
 };
 
 const SubscribeModal: React.FC<SubscribeModalProps> = ({ products }) => {
+  const { user, isLoading, subscription } = useUser();
+  const [priceIdLoading, setPriceIdLoading] = useState<string>();
+
+  const handleCheckout = async (price: Price) => {
+    setPriceIdLoading(price.id);
+
+    if (!user) {
+      setPriceIdLoading(undefined);
+      return toast.error("Must be Logged In to Subscribe.");
+    }
+
+    if (subscription) {
+      setPriceIdLoading(undefined);
+      return toast("Already Subscribed.");
+    }
+
+    try {
+      const { sessionId } = await postData({
+        url: "/api/created-checkout-session",
+        data: { price }
+      });
+
+      const stripe = await getStripe();
+      stripe?.redirectToCheckout({ sessionId });
+    } catch (error) {
+      console.error(error);
+      toast.error((error as Error)?.message);
+    } finally {
+      setPriceIdLoading(undefined);
+    }
+  };
+
   let content = <div className="text-center">No Products Available.</div>;
 
   if (products.length) {
@@ -33,13 +69,22 @@ const SubscribeModal: React.FC<SubscribeModalProps> = ({ products }) => {
           }
 
           return product.prices.map((price) => (
-            <Button key={price.id}>
+            <Button
+              key={price.id}
+              onClick={() => handleCheckout(price)}
+              disabled={isLoading || price.id === priceIdLoading}
+              className="mb-4"
+            >
               {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
             </Button>
           ));
         })}
       </div>
     );
+  }
+
+  if (subscription) {
+    content = <div className="text-center">Already Subscribed.</div>;
   }
 
   return (
