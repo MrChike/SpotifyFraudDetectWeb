6b3cdfc48f75eefb7e3228f53b192fbf2c0df8a6
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-02 12:10:56 +0530

    added helper funtion postData, toDateTime

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -11,3 +11,36 @@ export const getUrl = () => {
 
   return url;
 };
+
+export const postData = async ({
+  url,
+  data
+}: {
+  url: string;
+  data?: { price: Price };
+}) => {
+  console.log("POST REQUEST!", url, data);
+
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({
+      "Content-Type": "application/json"
+    }),
+    credentials: "same-origin",
+    body: JSON.stringify(data)
+  });
+
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+
+    throw Error(res.statusText);
+  }
+
+  return res.json();
+};
+
+export const toDateTime = (secs: number) => {
+  var time = new Date("1970-01-01T00:30:00Z"); // Unix Epoch Start.
+  time.setSeconds(secs);
+  return time;
+};
