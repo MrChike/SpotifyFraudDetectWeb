81429cf9fe0845b1f0882575366b788f91e692e7
chike <chikeegonu@gmail.com>
2025-03-11 22:31:50 +0100

    database types update

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
   apiVersion: "2022-11-15",
-  appInfo: { name: "Spotify Clone", version: "0.1.0" }
+  appInfo: { name: "Spotify (Fraud Detection)", version: "0.1.0" }
 });
